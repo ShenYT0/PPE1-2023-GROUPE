4648f88915044b80dd675c4aff2bc26a1dac4b14
ShenYT0 <shenyuntian0@gmail.com>
2023-11-29 11:45:48 +0100

    script jpn corrigé

---
+++ programmes/clear_all_jpn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm ../*/japonais/*
+rm ../tableaux/tableau_jpn.html
--- programmes/script_jpn.sh
+++ programmes/script_jpn.sh
@@ -9,16 +9,20 @@ echo "
 <html>
 <head>
 	<meta charset=\"UTF-8\">
-    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\">
+    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN\" crossorigin=\"anonymous\">
+    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL\" crossorigin=\"anonymous\"></script>
     <title>Tableau JPN</title>
 </head>
 <body>
-	<table class=\"table\">
+    <div class="table-responsive">
+	<table class=\"table table-info table-hover table-sm\">
         <tr>
             <th>num</th>
             <th>url</th>
             <th>requête</th>
             <th>encodage</th>
+            <th>aspiration</th>
+            <th>dump-text</th>
             <th>compte</th>
             <th>contexte</th>
             <th>concordance</th>
@@ -28,42 +32,56 @@ echo "
 var=1
 while read -r line;
 do
-    curl -s -o "../aspirations/japonais/jp-$var.html" "$line" #asp
-    lynx -dump -nolist "$line" > "../dumps-text/japonais/jp-$var.txt" #dump
-    
-    grep -A 1 -B 1 --color "食の安全" "../dumps-text/japonais/jp-$var.txt" > "../contextes/japonais/jp-$var.txt"
-
     if [ -f "../concordances/japonais/jp-$var.html" ];
     then
         rm "../concordances/japonais/jp-$var.html"
     fi
 
-    echo "
-    <html>
-    <head>
-        <meta charset=\"UTF-8\">
-        <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\">
-        <title>Concordance JPN $var</title>
-    </head>
-    <body>
-        <table class=\"table\">
-            <tr>
-                <th>gauche</th>
-                <th>mot</th>
-                <th>droite</th>
-            </tr>
-        ">>"../concordances/japonais/jp-$var.html"
-    grep -E -T -i "食の安全" ../contextes/japonais/jp-$var.txt | sed -E 's/(.*)(食の安全)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/'>>"../concordances/japonais/jp-$var.html"
-    echo "
-        </table>
-    </body>
-    </html>
-    ">>"../concordances/japonais/jp-$var.html"
-    
 
-    compte=$(grep -c "食の安全" "../dumps-text/japonais/jp-$var.txt")
-    code=$(curl -s -I -L -w "%{http_code}" -o /dev/null $line) #$(curl -s -I $line | head -n 1)
+    code=$(curl -s -L -w "%{http_code}" -o "../aspirations/japonais/jp-$var.html" $line) #$(curl -s -I $line | head -n 1)
 	encodage=$(curl -s -I -L -w "%{content_type}" -o /dev/null $line | grep -P -o "charset=\S+" | cut -d "=" -f2) #$(curl -s -I $line | grep "content-type:")
+
+    #curl -s -o "../aspirations/japonais/jp-$var.html" "$line" #asp
+    dump="NA"
+
+    if [ $code == "200" ] #-eq 200
+    then
+        lynx -dump -nolist "../aspirations/japonais/jp-$var.html" > "../dumps-text/japonais/jp-$var.txt" #dump
+        compte=$(grep -i -o "食の安全" "../dumps-text/japonais/jp-$var.txt" | wc -l)
+        dump="<a href=\"../dumps-text/japonais/jp-$var.txt\">dump-text</a>"
+
+        grep -i -C 3 "食の安全" "../dumps-text/japonais/jp-$var.txt" > "../contextes/japonais/jp-$var.txt"
+        #grep -A 1 -B 1 --color "食の安全" "../dumps-text/japonais/jp-$var.txt" > "../contextes/japonais/jp-$var.txt"
+        contexte="<a href=\"../contextes/japonais/jp-$var.txt\">contexte</a>"
+
+        echo "
+            <html>
+            <head>
+                <meta charset=\"UTF-8\">
+                <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\">
+                <title>Concordance JPN $var</title>
+            </head>
+            <body>
+                <table class=\"table\">
+                    <tr>
+                        <th>gauche</th>
+                        <th>mot</th>
+                        <th>droite</th>
+                    </tr>
+                ">>"../concordances/japonais/jp-$var.html"
+        
+        grep -E -T -i "食の安全" ../contextes/japonais/jp-$var.txt | sed -E 's/(.*)(食の安全)(.*)/<tr><td>\1<\/td><td>\2<\/td><td>\3<\/td><\/tr>/'>>"../concordances/japonais/jp-$var.html"
+        echo "
+            </table>
+            </body>
+            </html>
+            ">>"../concordances/japonais/jp-$var.html"
+        concordance="<a href=\"../concordances/japonais/jp-$var.html\">concordance</a>"
+    else
+        compte=""
+        contexte=""
+        concordance=""
+    fi
 	
     #echo -e "$var\t$line\t$code\t$encodage\t"
 	
@@ -73,9 +91,11 @@ do
             <td><a href=\"$line\">$line</a></td>
             <td>$code</td>
             <td>$encodage</td>
+            <td><a href=\"../aspirations/japonais/jp-$var.html\">aspiration</a></td>
+            <td>$dump</td>
             <td>$compte</td>
-            <td><a href=\"../contextes/japonais/jp-$var.txt\">contexte</a></td>
-            <td><a href=\"../concordances/japonais/jp-$var.html\">concordance</a></td>
+            <td>$contexte</td>
+            <td>$concordance</td>
         </tr>
 	">>"../tableaux/tableau_jpn.html"
 	
@@ -85,6 +105,7 @@ done < "../URLs/urls_jpn.txt"
 
 echo "
 	</table>
+    </div>
 </body>
 </html>
 ">>"../tableaux/tableau_jpn.html"
