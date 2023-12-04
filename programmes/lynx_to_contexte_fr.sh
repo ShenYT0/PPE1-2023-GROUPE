351fd12f9ae829cd30cffbdcb4a00b25968d14b2
Weiqi ZHANG <weiqi.zhang_arthur@yahoo.com>
2023-12-04 20:36:35 +0100

    添加了法语的一些dump contexte等文件

---
+++ programmes/lynx_to_contexte_fr.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#lynx每个链接，找关键词，显示前一行后一行，把结果储存到contextes文件夹里，重复50次
+
+URLS=$1
+numero_fichier=1
+
+while read -r ASPIR
+do
+    lynx -dump -nolist -assume_charset=utf-8 --display_charset=utf-8 $ASPIR | tr '[:upper:]' '[:lower:]' | grep -B 2 -A 2 "sécurité alimentaire" > ../contextes/fr/fr_${numero_fichier}.txt
+	numero_fichier=$(expr $numero_fichier + 1)
+done < "$URLS"
